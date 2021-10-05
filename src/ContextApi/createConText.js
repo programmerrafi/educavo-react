e1a5a81b28dc47d08818be272ea438d6066bf139
mdrafi10 <79692008+mdrafi10@users.noreply.github.com>
2021-10-05 13:31:34 +0600

    Update createConText.js

--- src/ContextApi/createConText.js
+++ src/ContextApi/createConText.js
@@ -20,7 +20,7 @@ export function CreateConText({ children }) {
   // navbar useEffect
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 0) {
+      if (window.scrollY > 50) {
         setShowNav(true);
       } else {
         setShowNav(false);
